96c382ff92fed271cd30b92ff94d421f44103c79
sezen <sezen.egor@mail.ru>
2026-03-04 22:53:49 +0300

    feat(datasets): register Dataset and DatasetRow in admin

--- apps/datasets/admin.py
+++ apps/datasets/admin.py
@@ -1,1 +1,28 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Dataset, DatasetRow
+
+
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "file_type",
+        "status",
+        "user",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "file_type",
+    )
+    search_fields = ("name",)
+
+
+@admin.register(DatasetRow)
+class DatasetRowAdmin(admin.ModelAdmin):
+    list_display = (
+        "dataset",
+        "row_index",
+    )
+    list_filter = ("dataset",)
