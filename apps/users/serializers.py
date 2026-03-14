a444377647a930a44a5f42d85bda47185a3f9b6a
Egor <113209570+KuPriv@users.noreply.github.com>
2026-03-14 20:12:40 +0300

    fix(users): guard registration against TOCTOU race condition (#30)

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 
 from .models import User
@@ -16,18 +17,25 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["email", "password", "password2"]
+        # disable default UniqueValidator - handle manually in validate_email
+        # (fast path) and create (race condition guard)
         extra_kwargs = {"email": {"validators": []}}
 
-    def create(self, validated_data):
-        validated_data.pop("password2")
-        return User.objects.create_user(**validated_data)
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("Данный email уже зарегистрирован")
+        return value
 
     def validate(self, data):
         if data["password"] != data["password2"]:
             raise serializers.ValidationError({"password": "Пароли не совпадают"})
         return data
 
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Данный email уже зарегистрирован")
-        return value
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        try:
+            return User.objects.create_user(**validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {"email": "Данный email уже зарегистрирован"}
+            )
